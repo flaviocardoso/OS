478ec1b8d0b4a9d389319f7b996bce960df6ab63
flaviocardoso <flavioc41@gmail.com>
2019-03-20 16:01:49 -0300

    Só carrefa sesões quando o usuário for autorizado

--- app/data/session_user.php
+++ app/data/session_user.php
@@ -18,31 +18,35 @@ $count = $rwc['count'];
 $saida = array('user' => $in['user']);
 // usuário com grupo
 if ($count > 0 && $rows['ativo'] == 1) {
-	new Session($rows);
+	$flag = true;
 	$out['uid'] = $in['uid'];
 	list($nome, $sobrenome) = explode(" ", $rows['nome']);
 	switch ($rows['grupo']) {
-		case 'admin':			
+		case 'admin':
 		case 'secr':
 			$userSuper = ($rows['grupo'] == 'admin') ? "Administrador: " : "Secretária(o): ";
-			switch ($rows['S']) {				
-				case 1:										
+			switch ($rows['S']) {
+				case 1:
 					$out['message'] = $userSuper . $nome . " " . $sobrenome . "<br />logado!";
-					break;				
+					break;
 				default:
 					$out['uid'] = NULL;
 					$out['error'] = true;
 					$out['message'] = $userSuper . 'não autorizado!';
+					$flag = false;
 					break;
-			}			
+			}
 			break;
-		
-		case 'resp':			
+
+		case 'resp':
 		case 'tec':
 		case 'sol':
 			$user = ($rows['grupo'] == 'sol') ? "Solicitante: " : "Técnico: ";
 			$out['message'] = $user . $nome . " " . $sobrenome . "<br />logado!";
 			break;
+	}
+	if (flag) {
+		new Session($rows);
 	}
 }
 else {
@@ -57,8 +61,8 @@ else {
 		$out['message'] = "Solicitante: " . $nome . " " . $sobrenome . "<br />logado!";
 	} else {
 		$out['error'] = true;
-		$out['message'] = 'Usuário não encontrado!';
-	}	
+		$out['message'] = 'Usuário não autorizado!';
+	}
 }
 echo json_encode($out, JSON_PRETTY_PRINT);
 ?>
